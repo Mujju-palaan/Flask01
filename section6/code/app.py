f2d1ea119ff1e34efa7cbadc69ae22cec4103aff
Your Name <mdmujahid97@gmail.com>
2019-09-05 09:55:57 +0530

    flask project

---
+++ section6/code/app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask_restful import Api
+from security import authenicate, identity
+from flask_jwt import JWT 
+from resources.user import UserRegister
+from resources.item import Item, ItemList
+
+app = Flask(__name__)
+app.secret_key = 'mujju'
+api = Api(app)
+
+jwt = JWT(app, authenicate, identity)
+
+api.add_resource(Item, '/item/<string:name>') 
+api.add_resource(ItemList, '/items')
+api.add_resource(UserRegister, '/register')
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
